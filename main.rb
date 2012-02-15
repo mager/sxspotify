18ec027451ebdf0e30e8a3f18d584793e137100a
Andrew Mager <andrew+github@mager.co>
2012-02-15 15:04:42 -0500

    hardcode everything

--- main.rb
+++ main.rb
@@ -3,21 +3,16 @@ require 'sinatra'
 require 'twilio-ruby'
 
 # Twilio setup
-BROADCASTER = '+15128616593'
-@client = Twilio::REST::Client.new ENV['TWILIO_ID'], ENV['AUTH_TOKEN']
+SID = 'AC92f0c87900e80c41ecfde5f7e6a9f0e3'
+AUTH_TOKEN = '5ec9e80206da8b8779b370c73f74df65'
+@client = Twilio::REST::Client.new SID, AUTH_TOKEN
 
 get '/' do
   erb :main
 end
 
 post '/sms' do
-  puts params
-  @from = params[:From]
-  @message = 'Success!'
-  @client.account.sms.messages.create(
-    :from => '+15128616593',
-    :to => '+14158305533',
-    :body => 'Hey there!'
-  )
+  puts 'HELLO!'
+  @client.account.sms.messages.create(:from => '+15128616593',:to => '+14158305533',:body => 'Hey there!')
 end
 
