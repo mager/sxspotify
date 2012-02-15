e23fe178b0615ea41201e4d326948445ccb994fc
Andrew Mager <andrew+github@mager.co>
2012-02-15 14:06:12 -0500

    rm id

--- main.rb
+++ main.rb
@@ -5,7 +5,7 @@ require 'twiliolib'
 # Twilio setup
 API_VERSION = '2010-04-01'
 CALLER_ID = '(646) 448-9996'
-ACCOUNT_SID = 'AC92f0c87900e80c41ecfde5f7e6a9f0e3'
+ACCOUNT_SID = ENV['TWILIO_ID']
 ACCOUNT = Twilio::RestAccount.new(ENV['ACCOUNT_SID'], ENV['ACCOUNT_TOKEN'])
 URL_POST_SMS = "/#{API_VERSION}/Accounts/#{ENV['ACCOUNT_SID']}/SMS/Messages"
 
