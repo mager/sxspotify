58b40eab51fa0a63c38f0a83f27c12d6ea88b72e
Andrew Mager <mager@Andrews-MacBook-Air-3.local>
2012-02-15 13:55:29 -0500

    Test messaging

--- main.rb
+++ main.rb
@@ -1,7 +1,23 @@
 require 'rubygems'
 require 'sinatra'
+require 'twiliolib'
+
+# Twilio setup
+API_VERSION = '2010-04-01'
+CALLER_ID = '(646) 448-9996'
+ACCOUNT_SID = 'AC92f0c87900e80c41ecfde5f7e6a9f0e3'
+ACCOUNT = Twilio::RestAccount.new(ENV['ACCOUNT_SID'], ENV['ACCOUNT_TOKEN'])
+URL_POST_SMS = "/#{API_VERSION}/Accounts/#{ENV['ACCOUNT_SID']}/SMS/Messages"
 
 get '/' do
   erb :main
+end
+
+post '/sms' do
+  @from = params[:From]
+  @message = 'Success!'
+  h = {:From => '+15128616593', :To => @from, :Body => @message }
+  resp = ACCOUNT.request(URL_POST_SMS, 'POST', h)
+
 end
 
