4a9627de6e8d905dd7d75e89db773a20cbc50448
Andrew Mager <andrew+github@mager.co>
2012-02-15 20:48:11 -0500

    Get these gems workin

--- main.rb
+++ main.rb
@@ -3,19 +3,17 @@ require 'sinatra'
 require 'twilio-ruby'
 
 # Twilio setup
-SID = 'AC92f0c87900e80c41ecfde5f7e6a9f0e3'
-AUTH_TOKEN = '5ec9e80206da8b8779b370c73f74df65'
-@client = Twilio::REST::Client.new SID, AUTH_TOKEN
+@sid = 'AC92f0c87900e80c41ecfde5f7e6a9f0e3'
+@auth_token = '5ec9e80206da8b8779b370c73f74df65'
+@client = Twilio::REST::Client.new(@sid, @auth_token)
+@account = @client.account
 
 get '/' do
   erb :main
 end
 
 post '/sms' do
-  puts 'hello1'
-  puts @client
-  puts 'hello2'
-  @client.account.sms.messages.create(
+  @account.sms.messages.create(
     :from => '+15128616593',
     :to => '+14158305533',
     :body => 'Hey there!'
