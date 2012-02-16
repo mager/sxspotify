4edf267f0429b5cb3de7a475fe453710503faadc
Andrew Mager <andrew+github@mager.co>
2012-02-15 21:04:32 -0500

    Tried this \n \nhttp://stackoverflow.com/questions/8460208/cant-call-method-from-gem-twilio-ruby-appears-to-be-looking-in-my-controller

--- main.rb
+++ main.rb
@@ -6,7 +6,7 @@ require 'twilio-ruby'
 # Twilio setup
 @sid = 'AC92f0c87900e80c41ecfde5f7e6a9f0e3'
 @auth_token = '5ec9e80206da8b8779b370c73f74df65'
-@client = Twilio::REST::Client.new @sid, @auth_token
+@client = ::Twilio::REST::Client.new @sid, @auth_token
 
 get '/' do
   erb :main
