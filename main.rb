4c954d26215a83ded9c9aa1bf73ea2e4c4f97eab
Andrew Mager <andrew+github@mager.co>
2012-02-16 11:13:45 -0500

    Un-hardcode the message to go back to the sender

--- main.rb
+++ main.rb
@@ -10,9 +10,11 @@ post '/sms' do
   @sid = 'AC92f0c87900e80c41ecfde5f7e6a9f0e3'
   @auth_token = '5ec9e80206da8b8779b370c73f74df65'
   @client = Twilio::REST::Client.new @sid, @auth_token
+
+  @from = params[From]
   @client.account.sms.messages.create(
     :from => '+15128616593',
-    :to => '+14158305533',
+    :to => @from,
     :body => 'Hey there'
   )
 end
